2f24e465d527de3c7eec5a02a846ef0455decf01
Douglas Castro <eloydouglas00@gmail.com>
2026-09-25 10:19:54 -0300

    Concilia swagger e tabelade capital

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,28 @@
 import cors from "@fastify/cors";
 import Fastify from "fastify";
 import { routes } from "./routes/index.js";
+import { fastifySwagger } from "@fastify/swagger";
+import { fastifySwaggerUi } from "@fastify/swagger-ui";
 
 export async function buildApp() {
   const app = Fastify({ logger: false });
 
   await app.register(cors);
+
+  await app.register(fastifySwagger, {
+    openapi: {
+        info: {
+            title: "PoaLab: School Inventory",
+            version: "1.0.0",
+        },
+    },
+    // transform: jsonSchemaTransform,
+  });
+
+  await app.register(fastifySwaggerUi, {
+      routePrefix: "/docs",
+  });
+
   await app.register(routes, { prefix: "/api" });
 
   app.setErrorHandler((err, _request, reply) => {
